ed98805136fc36ffc843451f939d4fef30e5f987
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-03 15:26:12 -0600

    how many

--- playground.js
+++ playground.js
@@ -3,3 +3,10 @@ function plus(a,b){
     return arguments[0] + arguments[1];
 }
 console.log(plus(5,3));
+
+function howMany() {
+    return arguments['length'];
+}
+console.log("How Many:");
+console.log(howMany());
+console.log(howMany(3,4,2));
