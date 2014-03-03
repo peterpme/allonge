ee5044ffe4e4da9d880ef286e9c07ee0da669b48
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-03 15:36:58 -0600

    butFirst

--- playground.js
+++ playground.js
@@ -1,11 +1,5 @@
-var slice1 = [1,2,3,4,5].slice(0);
-// slice up to element 0 (slices nothing)
-console.log(slice1);
+function butFirst() {
+    return Array.prototype.slice.call(arguments,1,3);
+}
 
-var slice2 = [1,2,3,4,5].slice(1);
-// slice up to the second element (2,3,4,5)
-console.log(slice2);
-
-var slice3 = [1,2,3,4,5].slice(1,4);
-//slice from the 2nd element up to the 4th element (2,3,4)
-console.log(slice3);
+console.log(butFirst('a', 'b','c','d'));
