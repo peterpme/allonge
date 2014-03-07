cb376ab4c72a63e81f263bf6d2dfb7ee62305a3e
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-07 08:01:18 -0600

    memoMe practice

--- playground.js
+++ playground.js
@@ -1,13 +1,29 @@
-function not(fn){
-    return function(argument){
-        return !fn(argument);
-    };
+function memoMe(fn){
+    var cache = {};
+    return function (x){
+        if (x in cache){
+            console.log(x + " in cache "+ cache[x]);
+        }
+        else {
+            cache[x] = fn(x);
+            console.log(x + " added to cache: "+ cache[x]);
+            
+        }
+        return cache[x];
+    }
 }
 
-function something(x){
-    return x != null;
-}
-var nothing = not(something);
-console.log(nothing);
-var ok = nothing(true);
-console.log(ok);
+var mem = memoMe(squared);
+var memsquared5 = mem(5);
+var memsquared15 = mem(15);
+var memsquared6 = mem(6);
+var memsquared5 = mem(5);
+var memsquared2 = mem(2);
+var memsquared15 = mem(1500);
+console.log(mem);
+console.log(memsquared5);
+console.log(memsquared15);
+console.log(memsquared6);
+console.log(memsquared5);
+console.log(memsquared2);
+console.log(memsquared15);
