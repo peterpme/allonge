63da75987541d39ff1fabe990f509bb9bd225a24
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-07 08:55:10 -0600

    closure practice

--- playground.js
+++ playground.js
@@ -1,29 +1,54 @@
-function memoMe(fn){
-    var cache = {};
-    return function (x){
-        if (x in cache){
-            console.log(x + " in cache "+ cache[x]);
-        }
-        else {
-            cache[x] = fn(x);
-            console.log(x + " added to cache: "+ cache[x]);
-            
-        }
-        return cache[x];
+var nums = [1,4,9];
+var roots = nums.map(Math.sqrt);
+
+function squared(x){
+    return x*x;
+x}
+
+function added(x){
+    return x+x;
+}
+
+console.log(roots);
+
+function mapWith(fn){
+    return function(array){
+        return array.map(fn);
     }
 }
 
-var mem = memoMe(squared);
-var memsquared5 = mem(5);
-var memsquared15 = mem(15);
-var memsquared6 = mem(6);
-var memsquared5 = mem(5);
-var memsquared2 = mem(2);
-var memsquared15 = mem(1500);
-console.log(mem);
-console.log(memsquared5);
-console.log(memsquared15);
-console.log(memsquared6);
-console.log(memsquared5);
-console.log(memsquared2);
-console.log(memsquared15);
+var squareAll = mapWith(squared);
+var numArray = squareAll(nums);
+console.log(numArray);
+
+console.log("===========");
+function a(a){
+    console.log("1a: "+a);
+    return function(b){
+        console.log("1b: "+b);
+        console.log(arguments[0]+ " " + arguments[1]);
+        return function(c){
+            console.log("1c: " +c);
+            console.log(arguments[0] + " " + arguments[1]);
+            return a+b;
+        }
+    }
+}
+var b = a(5);
+var c = b(3,6);
+var d = c(2);
+console.log("a: " + a);
+console.log("b: " + b);
+console.log("c: " + c);
+console.log("d: " + d);
+console.log("====");
+var e = function(a){
+    return function(b){
+        return a+b;
+    }
+}
+var ee = e(5);
+var ff = ee(6);
+console.log("e:" +e);
+console.log("ee:" +ee);
+console.log("ff:" +ff);
