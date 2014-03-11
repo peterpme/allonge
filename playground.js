52d83eb69ab396a527e073b7b708c4df27907f0b
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-10 21:20:43 -0500

    variadic function 1

--- playground.js
+++ playground.js
@@ -1,10 +1,39 @@
-// String.prototype.repeat = function(count){
-//     var that = this;
-//     return Array.apply(void 0, Array(count)).reduce(function(p,c,n){return (n === 0) ? that :  p + that;})
-// }
+var __slice = Array.prototype.slice; 
 
-String.prototype.repeat = function(count) {
-    return Array(count+1).join(this);
+function variadic(fn){
+    var fnLength = fn.length;
+    
+    if (fnLength !=1){
+        return fn;
+    }
+    else if(fnLength ===1){
+        return function(){
+            return fn.call(this, __slice.call(arguments,0));
+        }
+    }
+    else {
+        return function(){
+            var numberOfArgs = arguments.length,
+            namedArgs = __slice.call(arguments,0,fnLength-1),
+            numberOfMissingNamedArgs = Math.max(fnLength - numberOfArgs -1,0),
+            argPadding = new Array(numberOfMissingNamedArgs),
+            variadicArgs = __slice.call(arguments. fn.length-1);
+            
+            return fn.apply(this, namedArgs.concat(argPadding).concat([variadicArgs]));
+        }
+    }
+};
+
+function unary(first){
+    return first;
 }
+console.log(unary('why', 'hello', 'there'));
 
-console.log("Tom".repeat(5));
+console.log(variadic(unary)('why', 'hello', 'there'));
+
+function binary(first, rest){
+    return [first,rest];
+}
+console.log(binary('why','hello','there'));
+
+console.log(variadic(binary)('why','hello','there'));
