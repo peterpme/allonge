552c0655856db25a3faf11ab59d2d6055e76e5bc
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-12 19:27:53 -0500

    unary functions

--- playground.js
+++ playground.js
@@ -1,29 +1,14 @@
-var __slice = Array.prototype.slice;
-var variadic = function(fn){
-    var count = Math.max(fn.length-1,0);
-    
-    return function() {
-        var args = __slice.call(arguments,0,count);
-        
-        args.length = count;
-        args.push(__slice.call(arguments,count));
-        
-        return fn.apply(this,args);
-    };
-};
+var a = ['1','2','3','4'];
 
-var fn = variadic(function(args){
-    return args;
+a.map(function(element,index,arr){
+    console.log({element:element,index:index,arr:arr})
 });
 
-console.log(fn());
-console.log(fn('a'));
-console.log(fn('a','b'));
-
-var fn = variadic(function(a,b, args){
-    return { a:a,b:b, args:args};
-});
-
-console.log(fn());
-console.log(fn('a'));
-console.log(fn('a','b'));
+function unary(fn){
+    if(fn.length ==1){
+        return fn;
+    }
+    else return function(something){
+        return fn.call(this,something);
+    }
+}
