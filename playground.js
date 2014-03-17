703100ff3d72781af27a8145f0fe1e573471939b
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-17 10:39:25 -0500

    arguments and references

--- playground.js
+++ playground.js
@@ -1,18 +1,12 @@
-function maybe(fn){
-    return function() {
-        var i;
-        
-        if (arguments.length === 0){
-            return
-        }
-        else {
-            for (i=0;i<arguments.length;++i){
-                if(arguments[i]== null) return;
-            }
-            return fn.apply(this,arguments);
-        }
-    }
-}
-var checksForSomething = maybe(function(value){
-    console.log(value);
-});
+var x = 'June 14, 1962',
+    y=x;
+
+console.log(x==y);
+
+var x = [2012,6,14],
+y=x;
+console.log(x==y);
+
+console.log((function (y){
+    return x==y;
+})(x));
