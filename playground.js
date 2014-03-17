c9467a6fc0e74caf3434173c4018be23347ecaa0
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-17 13:39:09 -0500

    mutations and aliases

--- playground.js
+++ playground.js
@@ -1,11 +1,3 @@
-console.log(
-
-(function(){
-    var age = 49;
-    (function(){
-        age = 50;
-    })();
-    return age;
-})()
-
-);
+var oneTwoThree = [1,2,3];
+oneTwoThree[0] = 'one';
+console.log(oneTwoThree);
