cc7c1c3614f434c82f355e21bdb0216d98a8af3f
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-17 13:08:50 -0500

    objects and functions

--- playground.js
+++ playground.js
@@ -10,3 +10,15 @@ x = 5;
 console.log(o['a']===o.a);
 console.log(o['a']);
 
+
+var years = {
+    year:2012,
+    month:6,
+    day:14,
+    
+    abs: function(a){
+        return a<0 ? -a : a;
+    }
+}
+console.log(years['day']===years.day);
+console.log(years.abs(5));
