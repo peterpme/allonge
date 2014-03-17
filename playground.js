3b272dc3bda9de5a4c5584e4b0c9aab419713d7f
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-17 14:04:41 -0500

    objects and references

--- playground.js
+++ playground.js
@@ -1,3 +1,20 @@
 var oneTwoThree = [1,2,3];
 oneTwoThree[0] = 'one';
+oneTwoThree[3] = 'four';
 console.log(oneTwoThree);
+
+var names = {
+    firstName: 'hi',
+    lastName: 'hello'
+};
+
+names.middleName= 'err';
+console.log(names);
+
+
+var objs = {
+    a: {a:1,b:2},
+    b: {a:3,b:4},
+    c:'hello'
+};
+console.log(objs);
