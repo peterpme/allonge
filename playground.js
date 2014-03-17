d957c267f883311f7f51a1f9c8765cc3e038f69a
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-17 13:19:16 -0500

    shadowing variable closure

--- playground.js
+++ playground.js
@@ -1,24 +1,11 @@
-var unique = function() {
-    return function() {}
-},
-x = unique(),
-y = unique(),
-z = unique(),
-o = { a:x,b:y, c:z};
-console.log(o['a'] === x && o['b'] === y && o['c']===z);
-x = 5;
-console.log(o['a']===o.a);
-console.log(o['a']);
+console.log(
 
+(function(){
+    var age = 49;
+    (function(){
+        var age = 50;
+    })();
+    return age;
+})()
 
-var years = {
-    year:2012,
-    month:6,
-    day:14,
-    
-    abs: function(a){
-        return a<0 ? -a : a;
-    }
-}
-console.log(years['day']===years.day);
-console.log(years.abs(5));
+);
