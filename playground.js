2a943f89b3e2ce3fd681cb529c086a042d2997b6
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-03-19 16:29:29 -0500

    new js

--- playground.js
+++ playground.js
@@ -1,17 +1,7 @@
-var words = 'cat cat cat dog shoot dog dog cat cat';
-
-function countAdjacentPairs(searchString) {
-  var arrs = searchString.split(" "),
-      count = 0;
-      
-  arrs.map(function(c,i,a){
-      p=arrs[i+1];
-      if(c===p){
-          count++;
-      }
-      
-  });
-  return count;
+var even = function(num){
+  return (num === 0) || !(even(num-1));
+ 
 }
 
-console.log(countAdjacentPairs(words));
+
+console.log(even(5));
