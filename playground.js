05edff99892b0f798fcd177c1a0b4858d8c00bc6
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-04-01 20:16:01 -0500

    once

--- playground.js
+++ playground.js
@@ -1,37 +1,26 @@
-var operations = 
-(function(){
-  var even = function(num){
-    return (num===0) || odd(num-1)
-  };
-  
-  var odd = function(num){
-    return (num > 0) && even(num-1)
-  };
-  
-  return {
-    even: even,
-    odd: odd
-  }
-})(),
-even = operations.even,
-odd = operations.odd;
+// once
 
-console.log(even(1));
-console.log(odd(5));
-
-// transient let: 
-/* nothing we bind in let is returned from let or otherwise 
-passed out through assignment 
-so th eenvironment of the let is discarded 
-when let finishes being evaluated */
-var factorialOfTwentyFive = 
-(function(){
-  var factorial = function(num){
-    if (num<2){
-      return 1;
-    }
-    else return num * factorial(num-1);
+function once(fn){
+  var done = false;
+  
+  return function(){
+    return done ? void 0 : ((done = true), fn.apply(this, arguments));
   }
-  return factorial(25);
-})();
-console.log(factorialOfTwentyFive);
+}
+
+function addThis(num){
+  return num + num;
+}
+
+var addMe = once(addThis);
+
+
+
+console.log(addMe(4));
+console.log(addMe(4));
+
+var askedOnBlindDate = once(function(){
+  return "sure, why not?";
+})
+
+console.log(askedOnBlindDate());
