2787ba5f536a65a03ec42df0b53758f89ae23d50
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-05-15 18:11:58 -0500

    mapwith

--- playground.js
+++ playground.js
@@ -13,3 +13,22 @@ var inventory = {
 };
 
 console.log(getWith('oranges')(inventory));
+
+var inventories = [
+  { apples: 0, oranges: 144, eggs: 36},
+  { apples:240, oranges: 100, eggs:10},
+  { apples:100, oranges:99, eggs:12}
+
+];
+
+function mapWith(fn){
+  return function(list){
+    return Array.prototype.map.call(list, function(something){
+      return fn.call(this,something);
+    });
+  };
+};
+
+function pluckWith(attr){
+  return mapWith(getWith(attr));
+}
