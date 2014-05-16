ca48132200cfbc4eea970777a9fa1bb2b5352a4b
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-05-16 07:55:16 -0500

    traverse a tree of random crap

--- playground.js
+++ playground.js
@@ -1,25 +1,6 @@
-// function getWith
-
-function getWith(attr){
-  return function(obj){
-    return obj[attr];
-  }
-}
-
-var inventory = {
-  apples:0,
-  oranges: 144,
-  eggs:36
-};
-
-console.log(getWith('oranges')(inventory));
-
-var inventories = [
-  { apples: 0, oranges: 144, eggs: 36},
-  { apples:240, oranges: 100, eggs:10},
-  { apples:100, oranges:99, eggs:12}
-
-];
+var report=
+[[{price:1.99,id:1},{price:4.99,id:2},{price:7.99,id:3},{price:1.99,id:4},{price:2.99,id:5},{price:6.99,id:6}],[{price:5.99,id:21},{price:1.99,id:22},{price:1.99,id:23},{price:1.99,id:24},{price:5.99,id:25}],
+[{price:7.99,id:221},{price:4.99,id:222},{price:7.99,id:223},{price:10.99,id:224},{price:9.99,id:225},{price:9.99,id:226}]];
 
 function mapWith(fn){
   return function(list){
@@ -32,3 +13,22 @@ function mapWith(fn){
 function pluckWith(attr){
   return mapWith(getWith(attr));
 }
+
+function getWith(attr){
+  return function(obj){
+    return obj[attr];
+  }
+}
+
+function deepMapWith(fn){
+  return function innerdeepMapWith(tree){
+    return Array.prototype.map.call(tree, function(element){
+      if (Array.isArray(element)){
+        return innerdeepMapWith(element);
+      }
+      else return fn(element);
+    });
+  };
+};
+
+console.log(deepMapWith(getWith('price'))(report));
