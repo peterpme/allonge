2bb8751b8caf9a45db4d49dbb7205353955a5289
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-05-16 08:03:50 -0500

    basic constructor logic

--- playground.js
+++ playground.js
@@ -1,34 +1,28 @@
-var report=
-[[{price:1.99,id:1},{price:4.99,id:2},{price:7.99,id:3},{price:1.99,id:4},{price:2.99,id:5},{price:6.99,id:6}],[{price:5.99,id:21},{price:1.99,id:22},{price:1.99,id:23},{price:1.99,id:24},{price:5.99,id:25}],
-[{price:7.99,id:221},{price:4.99,id:222},{price:7.99,id:223},{price:10.99,id:224},{price:9.99,id:225},{price:9.99,id:226}]];
+var Ur = function(){};
+console.log((function() {}).prototype=== (function() {}).prototype);
 
-function mapWith(fn){
-  return function(list){
-    return Array.prototype.map.call(list, function(something){
-      return fn.call(this,something);
-    });
-  };
-};
+Ur.prototype.language="Javascript";
 
-function pluckWith(attr){
-  return mapWith(getWith(attr));
-}
+var continent = new Ur();
+console.log(continent.language);
 
-function getWith(attr){
-  return function(obj){
-    return obj[attr];
-  }
-}
+continent.language = "CoffeeScript";
+console.log(continent);
+console.log(continent.language);
+console.log(Ur.prototype.language);
 
-function deepMapWith(fn){
-  return function innerdeepMapWith(tree){
-    return Array.prototype.map.call(tree, function(element){
-      if (Array.isArray(element)){
-        return innerdeepMapWith(element);
-      }
-      else return fn(element);
-    });
-  };
-};
+// New instances don't acquire any changes made to other instances.
 
-console.log(deepMapWith(getWith('price'))(report));
+var another = new Ur();
+
+console.log(another.language);
+
+Ur.prototype.language = 'SUmerian';
+
+// change behavior of contructors prototype changes behavior of all instances
+console.log(another.language);
+
+console.log(continent.constructor);
+
+console.log(Ur.prototype.constructor)
+console.log(Ur.prototype.constructor === Ur)
