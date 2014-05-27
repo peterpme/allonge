ae2592330dfb970d87a1db1c181d640b4a1d45de
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-05-27 11:16:02 -0500

    everything inherits some methods from its constructors prototypes

--- playground.js
+++ playground.js
@@ -1,27 +1,3 @@
-var Ur = function() {};
+console.log(3.123412311.toPrecision(5));
 
-new Ur();
-
-console.log(new Ur() === new Ur());
-
-console.log(Ur.prototype);
-
-Ur.prototype.language = "javascript";
-
-var continent = new Ur();
-console.log(continent.language);
-
-continent.language = "Coffeescript";
-console.log(continent);
-
-var another = new Ur();
-
-// new instances don't acquire changes made to othe rinstances
-
-console.log(another.language);
-
-// changes made to constructors prototype changes behavior of all its instances.
-
-console.log(continent.constructor);
-
-console.log(continent.constructor === Ur)
+console.log('FORTRON,SNOBOL,LISP,BASIC'.split(',').length);
