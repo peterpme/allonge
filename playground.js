c94df5f46ce4c4bf47ba5065a297d09af92a3f67
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-11 19:41:04 -0500

    eggs in inventories

--- playground.js
+++ playground.js
@@ -1,8 +1,42 @@
-// primitiveType(...) creates original primitive from a primitive or instance of primitive object
-console.log(String(new String("Space"))=== "Space");
-
-var original = function(unknown){
-  return unknown.constructor(unknown);
+function inventoryRecord (apples, oranges, eggs) {
+  this.record = {
+    apples: apples,
+    oranges: oranges,
+    eggs: eggs
+  }
 }
-console.log(original(true)===true);
-console.log(original(new Boolean(true))===true);
+
+inventoryRecord.prototype.apples = function apples() {
+  return this.record.apples;
+}
+
+inventoryRecord.prototype.oranges = function oranges() {
+  return this.record.oranges;
+}
+
+inventoryRecord.prototype.eggs = function eggs() {
+  return this.record.eggs;
+}
+
+var inventories = [
+   new inventoryRecord(0, 144, 36),
+   new inventoryRecord(240,54,12),
+   new inventoryRecord(24,12,42)
+];
+
+function mapWith(fn) {
+  return function (list) {
+    return Array.prototype.map.call(list, function (something) {
+      return fn.call(this, something);
+    });
+  };
+};
+
+function getWith(attr) {
+  return function (object){
+    return object[attr];
+  }
+}
+
+console.log(mapWith(getWith('eggs')(inventories)));
+console.log(inventories[0].record);
