85998b7c3dbbd0656e633cf02056a708bb1f04a0
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-11 19:46:59 -0500

    binding issues

--- playground.js
+++ playground.js
@@ -1,42 +1,15 @@
-function inventoryRecord (apples, oranges, eggs) {
-  this.record = {
-    apples: apples,
-    oranges: oranges,
-    eggs: eggs
-  }
+function myName() {
+  return this.name;
 }
 
-inventoryRecord.prototype.apples = function apples() {
-  return this.record.apples;
-}
+var harpo = { name: 'Harpo'},
+    chico = { name: 'Chico'},
+    groucho = { name: 'Groucho'};
+    
+var fh = myName.bind(harpo);
+console.log(fh());
 
-inventoryRecord.prototype.oranges = function oranges() {
-  return this.record.oranges;
-}
-
-inventoryRecord.prototype.eggs = function eggs() {
-  return this.record.eggs;
-}
-
-var inventories = [
-   new inventoryRecord(0, 144, 36),
-   new inventoryRecord(240,54,12),
-   new inventoryRecord(24,12,42)
-];
-
-function mapWith(fn) {
-  return function (list) {
-    return Array.prototype.map.call(list, function (something) {
-      return fn.call(this, something);
-    });
-  };
-};
-
-function getWith(attr) {
-  return function (object){
-    return object[attr];
-  }
-}
-
-console.log(mapWith(getWith('eggs')(inventories)));
-console.log(inventories[0].record);
+var fc = myName.bind(chico);
+console.log(fc());
+var fhg = fh.bind(groucho);
+console.log(fhg(), fc.call(groucho), fc.apply(groucho, []));
