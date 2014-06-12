4516a135d684ac64377c745df6c1efa278dfa4e3
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-11 19:53:41 -0500

    bind fix

--- playground.js
+++ playground.js
@@ -1,15 +1,29 @@
+var unbind = function unbind (fn) {
+  return fn.unbound ? unbind(fn.unbound()) : fn
+};
+function bind (fn, context, force) {
+  var unbound,bound;
+  
+  if (force) {
+    fn = unbind(fn);
+  }
+  bound = function() {
+    return fn.apply(context, arguments)
+  };
+  bound.unbound = function() {
+    return fn;
+  };
+  
+  return bound;
+}
+
 function myName() {
   return this.name;
 }
 
-var harpo = { name: 'Harpo'},
-    chico = { name: 'Chico'},
-    groucho = { name: 'Groucho'};
-    
-var fh = myName.bind(harpo);
-console.log(fh());
+var harpo = { name: 'harpo'},
+chico = { name: 'chicao'},
+groucho = { name: 'groucho'};
 
-var fc = myName.bind(chico);
-console.log(fc());
-var fhg = fh.bind(groucho);
-console.log(fhg(), fc.call(groucho), fc.apply(groucho, []));
+var fh = bind(myName, harpo);
+console.log(fh());
