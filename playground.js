38d897b52caf208e74d9dcc49c6c030ab0d12ebb
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-06-12 17:45:58 -0500

    new agnostic constructor pattern

--- playground.js
+++ playground.js
@@ -11,4 +11,22 @@ function withDefaultPassword() {
 
 var jeff = new User('tom', 'erickson');
 
-console.log(jeff);
+console.log(User('tom', 'catnip'));
+
+function User(name, password){
+  if (!(this instanceof User)) {
+    return new User(name, password);
+  }
+  this.name = name || "untitled";
+  this.password = password;
+};
+
+console.log(User('James', 'orange'));
+
+function withDefaultPassword(){
+  var args = Array.prototype.slice.call(arguments,0);
+  args[1] = 'swordfish';
+  return User.apply(this,args);
+}
+
+console.log(withDefaultPassword('James'));
